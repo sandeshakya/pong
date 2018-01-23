1eeef2159e0cc86116f4663fa739cedfd0f64e12
Sandesh Shakya <sas860@mail.usask.ca>
2018-01-23 15:20:38 -0600

    added font for score added power up sprites connected power up to world prints which paddle ball came from

---
+++ scripts/PowerUp.gd
@@ -0,0 +1,29 @@
+extends Area2D
+#dictionary of power (sprite_name: power_function)
+var power_dict = {}
+signal paddle_size_up
+signal paddle_size_down
+
+func _ready():
+	#functions for dictionary
+	var pdl_wide_func = funcref(self, "on_pdl_wide")
+	var pdl_small_func = funcref(self, "on_pdl_small")
+	
+	#initialize dictionary
+	power_dict["pdl_wide"] = pdl_wide_func
+	power_dict["pdl_small"] = pdl_small_func
+
+func _on_PowerUp_body_enter( body ):
+	if (body.get_name() == "ball"):
+		#call function based on current animation name
+		if (body.get_linear_velocity().x > 0):
+			power_dict[get_node("sprite").get_animation()].call_func("left")
+		else:
+			power_dict[get_node("sprite").get_animation()].call_func("right")
+
+func on_pdl_wide(paddle):
+	emit_signal("paddle_size_up", [paddle])
+	
+func on_pdl_small(paddle):
+	emit_signal("paddle_size_down", [paddle])
+	
--- scripts/world.gd
+++ scripts/world.gd
@@ -2,14 +2,13 @@ extends Node2D
 var left_position = Vector2()
 var right_position = Vector2()
 var middle_position = Vector2()
-var p1_score_position = Vector2()
-var p2_score_position = Vector2()
 onready var left = get_node("left")
 onready var right = get_node("right")
 onready var ball = get_node("ball")
 onready var separator = get_node("seperator")
 onready var p1_score_lbl = get_node("p1_score")
 onready var p2_score_lbl = get_node("p2_score")
+onready var pwr_up = get_node("PowerUp")
 var p1_score
 var p2_score
 
@@ -19,19 +18,19 @@ func _ready():
 	left_position = Vector2(size.x/10, size.y/2)
 	right_position = Vector2(size.x/10*9, size.y/2)
 	middle_position = Vector2(size/2)
-	p1_score_position = Vector2(0, 0)
-	p2_score_position = Vector2(size.x - 2*p2_score_lbl.get_size().x, 0)
 	
 	left.set_pos(left_position)
 	right.set_pos(right_position)
 	separator.set_pos(middle_position)
 	ball.set_pos(middle_position)
-	p1_score_lbl.set_pos(p1_score_position)
-	p2_score_lbl.set_pos(p2_score_position)
+	p1_score_lbl.set_pos(Vector2(5,5))
+	p2_score_lbl.set_pos(Vector2(size.x - 2*p2_score_lbl.get_size().x-5, 5))
 	
 	p1_score = 0
 	p2_score = 0
 	ball.connect("ball_out", self, "_on_ball_out")
+	pwr_up.connect("paddle_size_up", self, "_on_paddle_size_up")
+	pwr_up.connect("paddle_size_down", self, "_on_paddle_size_down")
 	set_fixed_process(true)
 
 func _on_ball_out(arg):
@@ -44,3 +43,9 @@ func _on_ball_out(arg):
 		p1_score_lbl.set_text(str(p1_score))
 	#center ball
 	ball.set_pos(middle_position)
+	
+func _on_paddle_size_up(args):
+	print("paddle size up " + args[0])
+
+func _on_paddle_size_down(args):
+	print("paddle size down " + args[0])
